05c5a94d1a0bb317430b8e7b4108e2214ad8713e
Konno <sdt2495sc0004@edu.sca.ac.jp>
2026-08-15 00:10:26 +0900

    仮の設定パネルに正確に開く閉じる機能とLB,RBの画像を追加

--- Assets/Konno/Script_Kon/MainMenu/SettingsPanel.cs
+++ Assets/Konno/Script_Kon/MainMenu/SettingsPanel.cs
@@ -3,27 +3,27 @@ using UnityEngine.InputSystem;
 
 public class SettingsPanel : MonoBehaviour
 {
-    [SerializeField] private MainMenuManager menu;
+    //[SerializeField] private MainMenuManager menu;
 
-    private void OnEnable()
-    {
-        gameObject.SetActive(true);
-    }
+    //private void OnEnable()
+    //{
+    //    gameObject.SetActive(true);
+    //}
 
-    private void Update()
-    {
-        if (Keyboard.current.escapeKey.wasPressedThisFrame ||
-            (Gamepad.current != null &&
-             Gamepad.current.buttonEast.wasPressedThisFrame))
-        {
-            Close();
-        }
-    }
+    //private void Update()
+    //{
+    //    if (Keyboard.current.escapeKey.wasPressedThisFrame ||
+    //        (Gamepad.current != null &&
+    //         Gamepad.current.buttonEast.wasPressedThisFrame))
+    //    {
+    //        Close();
+    //    }
+    //}
 
-    void Close()
-    {
-        gameObject.SetActive(false);
+    //void Close()
+    //{
+    //    gameObject.SetActive(false);
 
-        menu.CloseSettings();
-    }
+    //    menu.CloseSettings();
+    //}
 }
